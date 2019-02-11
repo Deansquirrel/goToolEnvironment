fa681e84131e53760b64f3f7b215134ca174a02b
yuanxs <yuanxs@95szzl.cn>
2019-02-11 17:46:06 +0800

    201902111744

---
+++ Net.go
@@ -0,0 +1,57 @@
+package goToolEnvironment
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+)
+
+type internetIPInfo struct {
+	Ip      string `json:"ip"`
+	Country string `json:"country"`
+	City    string `json:"city"`
+}
+
+//获取公网IP
+func GetInternetAddr() (string, error) {
+	resp, err := http.Get("https://ipconfig.io/json")
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	var result internetIPInfo
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return "", err
+	}
+	return result.Ip, nil
+}
+
+//获取局域网IP
+func GetIntranetAddr() (string, error) {
+	ips, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	addr := ""
+	for _, ip := range ips {
+		ipNet, ok := ip.(*net.IPNet)
+		if ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				if addr != "" {
+					addr = addr + "|"
+				}
+				addr = addr + fmt.Sprintf("%s", ipNet.IP.To4())
+			}
+		}
+	}
+	return addr, nil
+}
---
+++ Os.go
@@ -0,0 +1,33 @@
+package goToolEnvironment
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"syscall"
+)
+
+//获取操作系统类型
+func GetOsType() string {
+	return runtime.GOOS
+}
+
+//获取当前操作系统名称
+func GetOsName() string {
+	return runtime.GOOS
+}
+
+//获取当前操作系统版本号
+func GetOsVer() (string, error) {
+	version, err := syscall.GetVersion()
+	if err != nil {
+		return "", err
+	} else {
+		ver := fmt.Sprintf("%d.%d.%d", byte(version), uint8(version>>8), version>>16)
+		return ver, nil
+	}
+}
+
+func GetHostName() (string, error) {
+	return os.Hostname()
+}
